9f89e2ca889441ea04611d59cdd63278d7cb3e4b
jasonkim24601 <104212407+jasonkim24601@users.noreply.github.com>
2022-05-04 03:47:14 -0500

    Added Beautiful Soup and changed scrape to only scrape text instead of Javascript and HTML

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-import
+from bs4 import BeautifulSoup
 
 
 
@@ -9,8 +9,9 @@ def main():
     URL = "https://magic.wizards.com/en/articles/archive/mtgo-standings/modern-preliminary-2022-05-03"
     page = requests.get(URL)
 
-    print(page.text)
-
+    # print(page.text)
+    soup = BeautifulSoup(page.content, "html.parser")
+    print(soup.get_text())
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
