d78bd5d9421a6a6eb564ece7e58a36d300f80f9c
jasonkim24601 <104212407+jasonkim24601@users.noreply.github.com>
2022-05-12 15:05:12 -0500

    Change program to work with csv files instead of txt

--- CSV_To_txt.py
+++ CSV_To_txt.py
@@ -1,4 +1,4 @@
-
+from compare import *
 import pandas as pd
 
 
@@ -6,7 +6,11 @@ def CSV_to_txt():
     csvfile = pd.read_csv('cards.csv')
     nameColumn = list(csvfile['name'])
     e = '\n'.join(map(str, nameColumn))
+
     print(e)
+
+
+
     with open("cardnames.csv", "w", encoding="utf-8") as file:
         file.write(e)
 
--- compare.py
+++ compare.py
@@ -1,3 +1,6 @@
+import csv
+
+
 def compare():
     card_filename = 'cardnames.csv'
     scraped_filename = 'souptextdump.csv'
@@ -8,7 +11,26 @@ def compare():
     with open(scraped_filename, 'r', encoding="utf-8") as f:
         lines = f.readlines()
 
-        for line in lines:
-            for host in hosts:
-                if host in line:
-                    print(line)
+
+    for line in lines:
+        for host in hosts:
+            if host == line:
+
+                print(line)
+
+
+
+
+
+
+    # csvfile = open('cards.csv', 'r')
+    #
+    # csvreader = csv.reader(csvfile, delimiter=',')
+    # for line in csvreader:
+    #     print(line)
+    #
+    # csvfile.close()
+
+
+if __name__ == '__main__':
+    compare()
