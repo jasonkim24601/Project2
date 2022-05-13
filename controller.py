4e16593c08efe7f4ba1bf3d2c5d219b970239e3a
jasonkim24601 <104212407+jasonkim24601@users.noreply.github.com>
2022-05-13 04:04:18 -0500

    Created basic MVC model

---
+++ controller.py
@@ -0,0 +1,9 @@
+from PyQt5.QtWidgets import *
+from gui import Ui_MainWindow
+from webscraper import *
+from compare import *
+
+class Controller(QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setupUi(self)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from webscraper import *
 from CSV_To_txt import *
 from compare import *
+from controller import *
 
 # Original idea: https://www.youtube.com/watch?v=RvCBzhhydNk (Webscraping)
 # Additional features: GUI and Custom CSV generated from online card database to use as reference to compare against
@@ -11,8 +12,16 @@ from compare import *
 # https://magic.wizards.com/en/articles/archive/mtgo-standings/modern-challenge-2022-05-09?xd12
 # https://magic.wizards.com/en/articles/archive/mtgo-standings/modern-showcase-challenge-2022-05-08?xd28
 
+def main():
+    app = QApplication([])
+    window = Controller()
+    window.show()
+    app.exec()
+
+
 
 if __name__ == '__main__':
-    webscaper("https://magic.wizards.com/en/articles/archive/mtgo-standings/modern-preliminary-2022-05-03")
-    CSV_to_txt()
-    compare("souptextdump.csv")
+    # webscaper("https://magic.wizards.com/en/articles/archive/mtgo-standings/modern-preliminary-2022-05-03")
+    # CSV_to_txt()
+    # compare("souptextdump.csv")
+    main()
