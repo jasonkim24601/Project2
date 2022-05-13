8d6e5b19af45aac9dbb42e6bf75cd6357b2447c0
jasonkim24601 <104212407+jasonkim24601@users.noreply.github.com>
2022-05-13 06:04:08 -0500

    Documenataion added

--- url_regex.py
+++ url_regex.py
@@ -2,6 +2,11 @@ import re
 
 
 def url_regex(url):
+    """
+    Method to check if input parameter is a URL.
+    :param url: URL to run regex against
+    :return: 1 if it is a URL, 0 otherwise
+    """
     x = re.search("(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])", url)
 
     if x != None:
