cba385f8b78e82d0506efbfed2844f834c87d0db
jasonkim24601 <104212407+jasonkim24601@users.noreply.github.com>
2022-05-13 05:56:33 -0500

    Light reformatting, added more example URLs for users to try in comments

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-from webscraper import *
-from CSV_To_txt import *
-from write import *
+# Original idea: https://www.youtube.com/watch?v=RvCBzhhydNk (Webscraping)
+# Additional features: GUI, pandas library usage, regex
+
+
 from controller import *
 
-# Original idea: https://www.youtube.com/watch?v=RvCBzhhydNk (Webscraping)
-# Additional features: GUI and Custom CSV generated from online card database to use as reference to compare against
 
-
-
-# Alternate URLs of different tournaments:
+# Links to process:
+# https://magic.wizards.com/en/articles/archive/mtgo-standings/modern-preliminary-2022-05-03"
 # https://magic.wizards.com/en/articles/archive/mtgo-standings/modern-challenge-2022-05-09?xd12
 # https://magic.wizards.com/en/articles/archive/mtgo-standings/modern-showcase-challenge-2022-05-08?xd28
 
@@ -19,9 +17,5 @@ def main():
     app.exec()
 
 
-
 if __name__ == '__main__':
-    # webscaper("https://magic.wizards.com/en/articles/archive/mtgo-standings/modern-preliminary-2022-05-03")
-    # CSV_to_txt()
-    # compare("souptextdump.csv")
     main()
