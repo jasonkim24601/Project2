847f1a94935f06ef3687385f850326df640fa6e0
jasonkim24601 <104212407+jasonkim24601@users.noreply.github.com>
2022-05-13 04:01:06 -0500

    Adding GUI, created raw UI without anything hooked up to it and converted it to a .py file.

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from webscraper import *
 from CSV_To_txt import *
 from compare import *
 
+# Original idea: https://www.youtube.com/watch?v=RvCBzhhydNk (Webscraping)
+# Additional features: GUI and Custom CSV generated from online card database to use as reference to compare against
+
+
+
 # Alternate URLs of different tournaments:
 # https://magic.wizards.com/en/articles/archive/mtgo-standings/modern-challenge-2022-05-09?xd12
 # https://magic.wizards.com/en/articles/archive/mtgo-standings/modern-showcase-challenge-2022-05-08?xd28
