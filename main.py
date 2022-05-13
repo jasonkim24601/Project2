da8966fd57e3b0e73bd38aba786d4b591c5a0747
jasonkim24601 <104212407+jasonkim24601@users.noreply.github.com>
2022-05-13 06:24:40 -0500

    Light formatting, changed comments

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from controller import *
 
 
 # Links to process:
-# https://magic.wizards.com/en/articles/archive/mtgo-standings/modern-preliminary-2022-05-03"
+# https://magic.wizards.com/en/articles/archive/mtgo-standings/modern-preliminary-2022-05-03
 # https://magic.wizards.com/en/articles/archive/mtgo-standings/modern-challenge-2022-05-09?xd12
 # https://magic.wizards.com/en/articles/archive/mtgo-standings/modern-showcase-challenge-2022-05-08?xd28
 
